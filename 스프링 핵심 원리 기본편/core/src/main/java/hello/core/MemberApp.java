f9ae59665cd18e06370c6571feb0ef475a4523c8
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-08-08 14:58:02 +0900

    section6

--- 스프링 핵심 원리 기본편/core/src/main/java/hello/core/MemberApp.java
+++ 스프링 핵심 원리 기본편/core/src/main/java/hello/core/MemberApp.java
@@ -1,30 +1,25 @@
-package hello.core.order;
+package hello.core;
 
-import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class OrderApp {
+public class MemberApp {
 
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
-//        OrderService orderService = appConfig.orderService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
-        Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
+        Member member = new Member(1L, "mamberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-
-        System.out.println("order = " + order);
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
