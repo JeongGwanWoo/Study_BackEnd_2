1ea59a4b6737ec726b8f2ce6a1bb8813a94ced8b
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-10-01 16:46:27 +0900

    dto, yml 추가

--- 스프링 SpringSecurity + JWT/jwtlogin/src/main/java/studyloginjwt/dto/JwtToken.java
+++ 스프링 SpringSecurity + JWT/jwtlogin/src/main/java/studyloginjwt/dto/JwtToken.java
@@ -1,4 +1,14 @@
 package studyloginjwt.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
 public class JwtToken {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
 }
